fca880edf638da49f8e85a2960e5b2a955090b63
J2ghz <j2.00ghz@gmail.com>
2019-11-25 15:53:43 +0100

    workiong, humidity only, receive

--- src/WeatherStation/Collector/Worker.cs
+++ src/WeatherStation/Collector/Worker.cs
@@ -1,13 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MQTTnet;
-using MQTTnet.Client.Connecting;
 using MQTTnet.Client.Options;
+using MQTTnet.Diagnostics;
 using MQTTnet.Extensions.ManagedClient;
 
 namespace Collector
@@ -21,18 +19,22 @@ namespace Collector
             _logger = logger;
         }
 
-        public static Task WhenCanceled( CancellationToken cancellationToken)
-        {
-            var tcs = new TaskCompletionSource<bool>();
-            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
-            return tcs.Task;
-        }
-
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                
+
+                MqttNetGlobalLogger.LogMessagePublished += (s, e) =>
+                {
+                    var trace = $">> [{e.TraceMessage.Timestamp:O}] [{e.TraceMessage.ThreadId}] [{e.TraceMessage.Source}] [{e.TraceMessage.Level}]: {e.TraceMessage.Message}";
+                    if (e.TraceMessage.Exception != null)
+                    {
+                        trace += Environment.NewLine + e.TraceMessage.Exception.ToString();
+                    }
+
+                    Console.WriteLine(trace);
+                };
+
                 // Setup and start a managed MQTT client.
                 var options = new ManagedMqttClientOptionsBuilder()
                     .WithAutoReconnectDelay(TimeSpan.FromMilliseconds(500))
@@ -40,22 +42,27 @@ namespace Collector
                         .WithClientId("Client1")
                         .WithTcpServer("192.168.2.108").Build())
                     .Build();
-                
+
                 var mqttClient = new MqttFactory().CreateManagedMqttClient();
-                //mqttClient.ApplicationMessageProcessedHandler = new ApplicationMessageProcessedHandlerDelegate(args => Console.WriteLine($"msg: {args}"));
-                //mqttClient.ApplicationMessageSkippedHandler = new ApplicationMessageSkippedHandlerDelegate(args => Console.WriteLine($"msg: {args}"));
-                //mqttClient.SynchronizingSubscriptionsFailedHandler = new SynchronizingSubscriptionsFailedHandlerDelegate(args => Console.WriteLine($"e: {args.Exception}"));
-                //mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(args => Console.WriteLine($"e: {args.Exception}"));
-                //mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(args =>
-                //{
-                //    Console.WriteLine($"msg: {args.AuthenticateResult}");
-                //});
-                mqttClient.UseApplicationMessageReceivedHandler(msg => Console.WriteLine(msg.ApplicationMessage.ConvertPayloadToString()));
-                await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("#").Build());
+                mqttClient.UseApplicationMessageReceivedHandler(msg =>
+                    Console.WriteLine(msg.ApplicationMessage.ConvertPayloadToString()));
+                await mqttClient.SubscribeAsync(
+                    new[]
+                    {
+                        new TopicFilterBuilder().WithTopic("rtl_433/+/devices/+/+/+/humidity").Build()
+                    }
+                );
                 await mqttClient.StartAsync(options);
                 Console.WriteLine(mqttClient.IsConnected);
                 await WhenCanceled(stoppingToken);
             }
+        }
+
+        public static Task WhenCanceled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>) s).SetResult(true), tcs);
+            return tcs.Task;
         }
     }
 }
