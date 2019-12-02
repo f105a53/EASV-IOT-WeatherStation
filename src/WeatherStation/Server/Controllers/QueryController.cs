5e658f89953b67f0f19dfdbf43891ed552b6e76f
J2ghz <j2.00ghz@gmail.com>
2019-12-02 14:12:02 +0100

    client history

---
+++ src/WeatherStation/Server/Controllers/QueryController.cs
@@ -0,0 +1,30 @@
+﻿using WeatherStation.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WeatherStation.Server.Service;
+
+namespace WeatherStation.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QueryController : ControllerBase
+    {
+
+        private readonly QueryService _query;
+
+        public QueryController( QueryService query)
+        {
+            _query = query;
+        }
+
+        [HttpGet]
+        public async Task<List<IGrouping<string, Temperature>>> GetTemperatures()
+        {
+            return await _query.GetTemperatures();
+        }
+    }
+}
