9af6d47c0865fce163e27f28a06257e1f085ac10
J2ghz <j2.00ghz@gmail.com>
2019-12-15 13:45:00 +0100

    update quesryservice connection string

--- src/WeatherStation/Server/Service/QueryService.cs
+++ src/WeatherStation/Server/Service/QueryService.cs
@@ -16,21 +16,21 @@ namespace WeatherStation.Server.Service
         public QueryService()
         {
             _client = InfluxDBClientFactory.Create("https://eu-central-1-1.aws.cloud2.influxdata.com",
-                "HEjjtm6ewV0f8AjQ4S7ymvpPxGeqpjbFcyCCeQF-sYO7lvH60veVT3eqf_BabQEvsK_n3l5-AGsEGVcbxTGJfw=="
+                "epLboaxSf0aYI6FBjeAoFb-RXykUUvwoeTt62Fc6rT414BD94zO6-2KPu6NbM5MuMdwpfp5zEK3dvnHjIKAynQ=="
                     .ToCharArray());
         }
 
         public async Task<IDictionary<string, List<Humidity>>> GetHumidities()
         {
             var api = _client.GetQueryApi();
-            var data = await api.QueryAsync<Humidity>("from(bucket:\"humidity\") |> range(start:-12h) |> filter(fn: (r) => r._measurement == \"humidity\")","f35d566fb41e6546");
+            var data = await api.QueryAsync<Humidity>("from(bucket:\"humidity\") |> range(start:-12h) |> filter(fn: (r) => r._measurement == \"humidity\")","93a7785d1f9d8493");
             return data.GroupBy(d => d.Device).ToDictionary(x=>x.Key,x=>x.ToList());
         }
 
         public async Task<IDictionary<string, List<Temperature>>> GetTemperatures()
         {
             var api = _client.GetQueryApi();
-            var data = await api.QueryAsync<Temperature>("from(bucket:\"humidity\") |> range(start:-12h) |> filter(fn: (r) => r._measurement == \"temperature_C\")","f35d566fb41e6546");
+            var data = await api.QueryAsync<Temperature>("from(bucket:\"humidity\") |> range(start:-12h) |> filter(fn: (r) => r._measurement == \"temperature_C\")","93a7785d1f9d8493");
             return data.GroupBy(d => d.Device).ToDictionary(x=>x.Key,x=>x.ToList());
         }
 
