b071c3a9a5f8535666c3c6326052d10a4ab50db7
Mikkel1996DK <Mikkelljungberg@gmail.com>
2019-12-16 01:11:24 +0100

    Added temperature chart

--- src/WeatherStation/Client/wwwroot/javascript/charts.js
+++ src/WeatherStation/Client/wwwroot/javascript/charts.js
@@ -1,9 +1,9 @@
 ﻿google.charts.load('current', { packages: ['corechart'] });
-google.charts.setOnLoadCallback(drawSingleLineChart);
+//google.charts.setOnLoadCallback(drawSingleLineChart);
 
 
 function mergeTimeArrays(timeArray) {
-    let a = [timeArray[0][0]]; // First array first timestamp
+    let a = [timeArray[0][0]];
 
     for (let i = 1; i < timeArray[0].length; i++) {
         let p_val = undefined;
@@ -52,8 +52,6 @@ function formatData(headers, data) {
 
     let dataArray = [dataHeaders];
     for (let timeKey of Object.keys(timeGrouped)) {
-        //console.log('timeGrouped[timeKey]: ', timeGrouped[timeKey]);
-
         let timeArray = [];
 
         for (let recObj of timeGrouped[timeKey]) {
@@ -78,30 +76,10 @@ function formatData(headers, data) {
 
 function drawSingleLineChart(element_id, chart_name, chart_data) {
 
-    console.log('Element ID: ', element_id);
-    console.log('Chart Name: ', chart_name);
-    console.log('Chart Data: ', chart_data);
-
     let formatted = formatData(["DateTime", "Device", "Value"], chart_data);
-
-    //console.log('Formatted: ', JSON.stringify(formatted));
 
     var data = new google.visualization.arrayToDataTable(formatted);
     
-    //var data = new google.visualization.arrayToDataTable([
-    //    ['Time', 'dev1', 'dev2'],
-    //    ['20:00', 23, 100],
-    //    ['21:00', 22, 98],
-    //    ['22:00', 18, 60],
-    //    ['23:00', 15, 66],
-    //    ['00:00', 13, 75],
-    //    ['01:00', 8, 80],
-    //    ['02:00', 5, 85],
-    //    ['03:00', 4, 90],
-    //    ['04:00', 3, 95],
-    //    ['05:00', 2, 99]
-    //]);
-
     var options = {
         title: chart_name,
         curveType: 'function',
