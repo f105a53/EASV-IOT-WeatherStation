ad456cc5c3f53f6beeb1cd4f5eb875f5782fe47c
J2ghz <j2.00ghz@gmail.com>
2019-12-16 11:53:40 +0100

    timed service

---
+++ src/WeatherStation/Server/Service/TimedHostedService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeatherStation.Server.Service
+{
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private Timer _timer;
+        private readonly ILogger _logger = Log.ForContext<TimedHostedService>();
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.Information("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            executionCount++;
+
+            _logger.Information(
+                "Timed Hosted Service is working. Count: {Count}", executionCount);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.Information("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
